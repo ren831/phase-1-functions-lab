b4847e5879dfc857e7e09c246797e188c6fdcd3e
Rene Hernandez <rene83120@gmail.com>
2022-02-01 19:40:11 -0800

    commit tree cefe26e21ec16ac50af8a0b7883c431458d7ed2c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {}
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+}
+function distanceTravelledInFeet(start, destination) {}
